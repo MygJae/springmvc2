93b111a7b8f8d627e94ea764ac4b59a5bb94daff
MygJae <50861514+mygjae@users.noreply.github.com>
2023-11-28 17:00:26 +0900

    검증2 - Been Validation

--- src/main/java/hello/springmvc2/Springmvc2Application.java
+++ src/main/java/hello/springmvc2/Springmvc2Application.java
@@ -3,19 +3,28 @@ package hello.springmvc2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class Springmvc2Application {
+public class Springmvc2Application /*implements WebMvcConfigurer*/ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springmvc2Application.class, args);
 	}
-/*
-	@Bean
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasenames("messages", "errors");
-		messageSource.setDefaultEncoding("utf-8");
-		return messageSource;
+
+	/*
+        @Bean
+        public MessageSource messageSource() {
+            ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+            messageSource.setBasenames("messages", "errors");
+            messageSource.setDefaultEncoding("utf-8");
+            return messageSource;
+        }
+    */
+	/*
+	@Override
+	public Validator getValidator() {
+		return new ItemValidator();
 	}
-*/
+	 */
 }
---
+++ src/test/java/hello/springmvc2/validation/BeenValidationTest.java
@@ -0,0 +1,4 @@
+package hello.springmvc2.validation;
+
+public class BeenValidationTest {
+}
