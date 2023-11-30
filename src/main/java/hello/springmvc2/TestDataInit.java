726c01049c5cf1ee0fb2bd595d77725c8114f148
MygJae <50861514+mygjae@users.noreply.github.com>
2023-11-30 16:54:50 +0900

    로그인 처리1 - 쿠키,세션

--- src/main/java/hello/springmvc2/TestDataInit.java
+++ src/main/java/hello/springmvc2/TestDataInit.java
@@ -3,6 +3,8 @@ package hello.springmvc2;
 
 import hello.springmvc2.domain.item.Item;
 import hello.springmvc2.domain.item.ItemRepository;
+import hello.springmvc2.domain.member.Member;
+import hello.springmvc2.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,7 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -21,6 +24,13 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setName("test!");
+        member.setPassword("test!!");
+
+        memberRepository.save(member);
     }
 
 }
---
+++ src/main/java/hello/springmvc2/domain/login/LoginService.java
@@ -0,0 +1,32 @@
+package hello.springmvc2.domain.login;
+
+import hello.springmvc2.domain.member.Member;
+import hello.springmvc2.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *
+     * return null 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(loginId))
+                .orElse(null);
+        /*
+        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+        Member member = findMemberOptional.get();
+        if (member.getPassword().equals(password)) {
+            return member;
+        } else {
+            return null;
+        }
+        */
+    }
+}
